d8f5520a2af3ecd1796ab0d0a0ae27200ae46413
Ernst Widerberg <ernstwi@kth.se>
2019-11-28 14:12:50 +0100

    pocket-tool 1.2.1

--- pocket-tool.rb
+++ pocket-tool.rb
@@ -1,22 +1,11 @@
 class PocketTool < Formula
   desc "Article manager for Pocket"
   homepage "https://github.com/ernstwi/pocket-tool"
-  url "https://github.com/ernstwi/pocket-tool/archive/1.2.0.tar.gz"
-  sha256 "28e0c745c55a008c7aaf5eddc8f33928d1c9fd7520c3f4a88f2854cc1a6db14e"
+  url "https://github.com/ernstwi/pocket-tool/archive/1.2.1.tar.gz"
+  sha256 "bc8b3918df64a03078c27ee8252f00879a2835780d22a1918aa2d805a66c23e1"
   head "https://github.com/ernstwi/pocket-tool.git"
 
-  resource "jsmn" do
-      url "https://github.com/zserge/jsmn/archive/732d283ee9a2e5c34c52af0e044850576888ab09.tar.gz"
-      sha256 "7c25a7da1d36d3fe3071abb4efc9f2563d44a9f215520deed171874f5b353605"
-  end
-
   def install
-    resource("jsmn").stage {
-        system "make CFLAGS=-DJSMN_PARENT_LINKS"
-        buildpath.install "libjsmn.a"
-        buildpath.install "jsmn.h"
-    }
-
     system "make"
     bin.install "pocket-tool"
   end
