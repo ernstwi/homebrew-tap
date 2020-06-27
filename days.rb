5755981424d848f660d602361be2fd35bfa69f1e
Ernst Widerberg <ernstwi@kth.se>
2020-06-27 18:40:34 +0200

    days 1.6.0

--- days.rb
+++ days.rb
@@ -3,8 +3,8 @@ require "language/node"
 class Days < Formula
   desc "Journaling tool"
   homepage "https://github.com/ernstwi/days"
-  url "https://registry.npmjs.org/@ernstwi/days/-/days-1.5.1.tgz"
-  sha256 "65a86288248672181b3549a6ae211c158b4072513e8febbaf24dc51cb2235334"
+  url "https://registry.npmjs.org/@ernstwi/days/-/days-1.6.0.tgz"
+  sha256 "65c94850db3bdb781320bc5bba4fccfbd913723436c5eaa64af6baacd002800f"
 
   depends_on "node"
 
